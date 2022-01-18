d571eaf976630c008b47232d897b058b00ca121a
Shreyavish <37011949+Shreyavish@users.noreply.github.com>
2022-01-17 18:37:21 -0600

    Update ToDo.js

--- todo-frontend/src/components/ToDo.js
+++ todo-frontend/src/components/ToDo.js
@@ -10,39 +10,29 @@ import React from 'react'
             key={index}
           >
             
-                        <div>
-            
-
-                        
-                            <div  key={todo.id}>
-
-                               
+                    <div>    
+                    <div  key={todo.id}>         
                     <div class ="row mb-2">
-
-                   
-
-                    <div class = "col-2">
                         
+                    <div class = "col-2">   
                     <input type="checkbox"  onClick={() => completeTodo(todo.id)} />
                     </div>
                     <div class = "col-3">
-                        
-                    
                     {todo.text}
                     </div>
                     <div class = "col-2">
                     <button  class="btn btn-danger btn-sm" name="remove" key={todo.id} onClick={() => clearItemFromPending(todo.id)}>
-                                            remove
-                                            </button>
+                     remove
+                    </button>
                     </div>
 
                     <div class = "col-5">
-
                     </div>
     
                     </div>
-                            </div>
-                        </div>
+                           
+                    </div>
+                    </div>
                         
             </div>
 
